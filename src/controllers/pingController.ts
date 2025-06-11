ff19952772f95c502952978f69d5aabb8d09fc75
Pranav <lifeispranav@gmail.com>
2025-06-11 13:10:20 +0530

    Added Basic routing and ping Controller

---
+++ src/controllers/pingController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+export const pingCheck = (_: Request, res: Response) : void => {
+  res.status(200).json({
+    message: 'Ping Check Ok!',
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, { Express } from 'express';
 
 import serverConfig from './config/server.config';
+import apiRouter from './routes';
 
 const app: Express = express();
 
 app.get('/', (_, res) => {
   res.send('hello');
 });
+
+app.use('/api', apiRouter);
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server Started on http://localhost:${serverConfig.PORT}`);
